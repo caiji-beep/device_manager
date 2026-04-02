1fd87dd2488aef3ba227a12bae24dccad75ebf9c
caiji-beep <2978115384@qq.com>
2026-04-02 18:20:29 +0800

    chore: 更新 .gitignore 并清理忽略的文件

--- drivers/led/.led.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/lsz/linux/IMX6ULL/Projects/device_manager/drivers/led/led.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/lsz/linux/IMX6ULL/Projects/device_manager/drivers/led/led.ko /home/lsz/linux/IMX6ULL/Projects/device_manager/drivers/led/led.o /home/lsz/linux/IMX6ULL/Projects/device_manager/drivers/led/led.mod.o
