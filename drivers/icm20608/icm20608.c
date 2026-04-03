5a6690bea01bfb02bd4d1752ac39a17b32a6c9a9
caiji-beep <2978115384@qq.com>
2026-04-03 16:53:01 +0800

    icm20608驱动修改

--- drivers/icm20608/icm20608.c
+++ drivers/icm20608/icm20608.c
@@ -20,23 +20,28 @@ struct icm20608_dev
 {
     struct miscdevice mdev;
     struct mutex lock;
+    struct spi_device *spi;
 
 };
 static int icm20608_spi_probe(struct spi_device *spi)
 {
+    printk("icm20608 spi probe success!\n");
     return 0;
 }
 
 static int icm20608_spi_remove(struct spi_device *spi)
 {
+    printk("icm20608 spi remove success!\n");
     return 0;
 }
 
 
 /*传统匹配*/
 static const struct spi_device_id icm20608_spi_ids[] = {
-    {"lsz,icm20608", 0},
+    {"icm20608", 0},
     {}};
+
+MODULE_DEVICE_TABLE(spi, icm20608_spi_ids);
 
 /*设备树匹配*/
 static const struct of_device_id icm20608_of_match[] = {
@@ -54,6 +59,8 @@ static struct spi_driver icm20608_spi_driver = {
     .remove = icm20608_spi_remove,
     .id_table = icm20608_spi_ids,
 };
+
+MODULE_DEVICE_TABLE(of, icm20608_of_match);
 
 module_spi_driver(icm20608_spi_driver);
 
